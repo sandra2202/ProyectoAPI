b84c961640f6cad33ba71a3729516355aeec30ed
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-06-10 18:49:58 -0500

    Consumiendo una REST API con Fetch API

--- fetchapi/js/app.js
+++ fetchapi/js/app.js
@@ -1,6 +1,6 @@
 document.getElementById('txtBtn').addEventListener('click', cargarTXT);
 document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
-
+document.getElementById('apiBTN').addEventListener('click', cargarREST);
 
 function cargarTXT(){
     fetch('datos.txt')
@@ -33,4 +33,24 @@ function cargarJSON(){
         .catch(function(error){
             console.log(error);
         });
+}
+
+function cargarREST(){
+    fetch('https://picsum.photos/list')
+        .then (function(res){
+            return res.json();
+        })
+        .then(function(imagenes){
+            let html ='';
+            
+            imagenes.forEach(function(imagen){
+                    html += `
+                        <li>
+                            <a href="${imagen.post_url}>Ver Imagen</a>
+                            ${imagen.author}
+                        </li>
+                    `;
+            });
+            document.getElementById('resultado').innerHTML = html;
+        })
 }
